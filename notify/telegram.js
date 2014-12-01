9f9e03c7bab03b486e8ec8a2aa051fce8092199c
Leon Bentrup <leon@leonadi.de>
2014-12-01 18:21:39 +0100

    Fix typo

--- notify/telegram.js
+++ notify/telegram.js
@@ -6,6 +6,7 @@ exports.setup = function (config, on, off, cb) {
   telegram.stdout.on("data", function(data) {
     tg_buffer += data;
     if (tg_buffer.indexOf("\n") !== -1) {
+      if (tg_buffer.indexOf(">>>") !== -1) {
         on();
       }
       if (tg_buffer.indexOf(config.name + " online") !== -1) {
